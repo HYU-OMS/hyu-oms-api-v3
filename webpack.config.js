42dda0bc81789956c65c91360638b08f43a36959
Kihoon Han <khhan1993@gmail.com>
2018-12-24 00:34:47 +0900

    [test] Remove typescript from project

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,21 @@
 const path = require('path');
+// eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
 
-const entries = {};
-
-Object.keys(slsw.lib.entries).forEach(
-  key => (entries[key] = ['./source-map-install.js', slsw.lib.entries[key]])
-);
-
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  entry: entries,
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  entry: slsw.lib.entries,
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }],
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
-  },
-  target: 'node',
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ],
   },
 };
